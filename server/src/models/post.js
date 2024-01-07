5ad3335bb7028283aaac51c22f66c74d70450807
ensarsnr <dev.anise@outlook.com>
2024-01-07 23:10:08 +0300

    Created user and post schema

---
+++ server/src/models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  image: { type: String, required: true },
+  caption: { type: String },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      text: { type: String, required: true },
+    },
+  ],
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
---
+++ server/src/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
