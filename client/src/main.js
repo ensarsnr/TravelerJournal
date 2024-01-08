e9e71c1a0fd8ee95baeaede9bebf66e526dc67f1
ensarsnr <dev.anise@outlook.com>
2024-01-08 21:43:22 +0300

    Lib imported

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import i18n from "./plugins";
 import "./index.css";
 
 const app = createApp(App);
+
+// libraries
 app.use(router);
+app.use(i18n);
 
 app.mount("#app");
