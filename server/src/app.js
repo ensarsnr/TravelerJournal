514df7aaa73579e282c823d84aa67e85e6e83946
ensarsnr <dev.anise@outlook.com>
2024-01-08 15:36:32 +0300

    Add middleware, database connection, and auth routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+const dotenv = require("dotenv");
+
+// routes
+const authRoutes = require("./routes/auth");
+
+dotenv.config();
 
 const port = 3000;
 const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+// Endpointler
+
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
