1ffeb0ae9b5155075b75e6d4b5c7f3592605623b
ensarsnr <dev.anise@outlook.com>
2024-01-15 18:33:00 +0300

    Added api

--- client/src/views/auth/RegisterForm.vue
+++ client/src/views/auth/RegisterForm.vue
@@ -9,26 +9,27 @@
                     <form class="m-auto w-2/4">
                         <div>
                             <h1 for="first_name" class="block mb-5 text-2xl font-extrabold text-gray-900">
-                                {{ $t("messages.register") }} </h1>
-                            <input type="email"
+                                Kayıt Sayfası </h1>
+                            <input :value="email" @input="onChangeEmail"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  placeholder-gray-400 "
                                 placeholder="e-mail" required>
                         </div>
                         <div class="mt-5">
-                            <input type="password"
+                            <input type="text" :value="username" @input="onChangeUsername"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  placeholder-gray-400 "
-                                placeholder="Password" required>
+                                placeholder="username" required>
                         </div>
                         <div class="mt-5">
-                            <input type=""
+                            <input :value="password" @input="onChangePassword" type="text"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  placeholder-gray-400 "
-                                placeholder="John" required>
+                                placeholder="password" required>
                         </div>
                         <div class="mt-5">
-                            <button
+                            <button @click="handleSubmit"
                                 class="px-4 py-2 bg-slate-400 hover:bg-slate-500 duration-200 hover:duration-200 rounded-lg w-full text-white text-lg font-extrabold">Kayıt
                                 Ol</button>
                         </div>
+                        <router-link to="/login">asdasd</router-link>
                     </form>
                 </div>
                 <div class="p-10 text-sm text-slate-400">© 2024 Travler's Journey. All Right Reserved </div>
@@ -39,3 +40,45 @@
         </div>
     </div>
 </template>
+
+
+<script>
+import services from "../../services";
+
+export default {
+    data() {
+        return {
+            username: "",
+            email: "",
+            password: "",
+        }
+    },
+    methods: {
+        onChangeUsername(e) {
+            this.username = e.target.value;
+        },
+        onChangeEmail(e) {
+            this.email = e.target.value;
+        },
+        onChangePassword(e) {
+            this.password = e.target.value;
+        },
+        handleSubmit(e) {
+            e.preventDefault();
+            this.authRegister();
+        },
+        async authRegister() {
+            try {
+                const response = services.register(
+                    this.email,
+                    this.username,
+                    this.password,
+                );
+                console.log(`Register response: ${response}`);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+}
+</script>
