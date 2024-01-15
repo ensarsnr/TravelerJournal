7206dc6319c581a524c6697bd6d6f3247fb6989a
ensarsnr <dev.anise@outlook.com>
2024-01-15 18:31:31 +0300

    Updated

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,26 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 import i18n from "./plugins";
 import "./index.css";
+import { createWebHistory } from "vue-router";
+import LoginFormVue from "./views/auth/LoginForm.vue";
+import RegisterFormVue from "./views/auth/RegisterForm.vue";
+import { createRouter } from "vue-router";
 
 const app = createApp(App);
 
+const routes = [
+  { path: "/", component: RegisterFormVue },
+  { path: "/login", component: LoginFormVue },
+];
+
+const customRouter = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
 // libraries
-app.use(router);
+app.use(customRouter);
 app.use(i18n);
 
 app.mount("#app");
