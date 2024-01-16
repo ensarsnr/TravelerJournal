89b32528d075c2d5c765705ae47446f9df0f25d3
ensarsnr <dev.anise@outlook.com>
2024-01-16 11:14:21 -0500

    Added new route

--- client/src/main.js
+++ client/src/main.js
@@ -5,22 +5,28 @@ import "./index.css";
 import { createWebHistory } from "vue-router";
 import LoginFormVue from "./views/auth/LoginForm.vue";
 import RegisterFormVue from "./views/auth/RegisterForm.vue";
+import HomeView from "./views/HomeView.vue";
 import { createRouter } from "vue-router";
 
+// Create the Vue app instance
 const app = createApp(App);
 
+// Define your routes
 const routes = [
-  { path: "/", component: RegisterFormVue },
+  { path: "/", component: RegisterFormVue }, 
   { path: "/login", component: LoginFormVue },
+  { path: "/home", component: HomeView }
 ];
 
+// Create the router instance
 const customRouter = createRouter({
   history: createWebHistory(),
   routes,
 });
 
-// libraries
+// Use the router and i18n plugin in your app
 app.use(customRouter);
 app.use(i18n);
 
+// Mount the app to the element with the ID "app"
 app.mount("#app");
