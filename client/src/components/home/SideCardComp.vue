924d0e2a462f001c7cbb38113cea0ee96c8a3819
ensarsnr <dev.anise@outlook.com>
2024-01-16 11:14:03 -0500

    Created side card

---
+++ client/src/components/home/SideCardComp.vue
@@ -0,0 +1,12 @@
+<template >
+    <div class="border-2 border-solid  border-black col-span-2">
+        <div class="w-1/4 h-2/4 rounded-full bg-black">
+            <!-- Image eklenecek -->
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
