89e0f2bc3596e135fb56df1fa7d06da1a6bf6588
ensarsnr <dev.anise@outlook.com>
2024-01-17 15:24:40 -0500

    Bug fixed

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -7,13 +7,14 @@ module.exports = (req, res, next) => {
     if (!tokenHeader || !tokenHeader.startsWith("Bearer ")) {
       return res.status(401).json({ message: "Authentication Failed" });
     }
-    const token = tokenHeader.replace("Bear er", "");
+    const token = tokenHeader.replace("Bearer ", ""); // Doğru şekilde ayıklama yapılmalı
     // token'ı doğrula
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // doğrulanmış kullanıcı bigilerini req objesine ata.
+    // doğrulanmış kullanıcı bilgilerini req objesine ata.
     req.user = decoded;
     next();
   } catch (error) {
+    console.error(`Authentication error: ${error.message}`);
     return res.status(401).json({ message: "Authentication Failed" });
   }
 };
