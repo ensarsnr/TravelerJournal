cab1aef23ac9b23ef551e5b607c329a42a7da117
ensarsnr <dev.anise@outlook.com>
2024-01-17 12:33:47 -0500

    Updated

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,14 +4,19 @@
 
     <div class="container mx-auto mt-4">
       <div class="grid grid-cols-10 gap-4">
-        <div class="col-span-2 bg-gray-300 p-4"> <!-- Adjust background color and padding as needed -->
-          <!-- Content for the left column -->
+        <div class="col-span-2 p-4">
           <SideCard />
         </div>
-
-        <div class="col-span-8 bg-gray-500 p-4"> <!-- Adjust background color and padding as needed -->
-          <!-- Content for the main column -->
-          Main Content
+        <div class="col-span-8 p-4">
+          <div class="flex flex-col w-4/5 bg-black">
+            <div class="flex">
+              <div class="m-1">pp</div>
+              <div class="m-1">kullanıcıadı</div>
+            </div>
+            <div class="h-52 bg-orange-400">IMG</div>
+            <div>beğenme, kaydetme</div>
+            <div>beğeni saysı</div>
+          </div>
         </div>
       </div>
     </div>
@@ -21,7 +26,18 @@
 <script>
 import NavbarComp from "../components/home/NavbarComp.vue";
 import SideCard from "../components/home/SideCardComp.vue";
+
 export default {
   components: { NavbarComp, SideCard },
+
+  beforeRouteEnter(to, from, next) {
+    const userToken = localStorage.getItem("userToken");
+
+    if (!userToken) {
+      next("/");
+    } else {
+      next();
+    }
+  },
 };
 </script>
