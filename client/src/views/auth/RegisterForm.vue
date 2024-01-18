0741b8f7fe0abc59327f6157179d273a78ea529c
ensarsnr <dev.anise@outlook.com>
2024-01-18 11:04:31 -0500

    Updated

--- client/src/views/auth/RegisterForm.vue
+++ client/src/views/auth/RegisterForm.vue
@@ -41,7 +41,7 @@
               <input
                 :value="password"
                 @input="onChangePassword"
-                type="text"
+                type="password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400"
                 placeholder="password"
                 required
