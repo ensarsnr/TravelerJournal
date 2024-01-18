7abd81cf407ad0c3a59b6e0c759e9e767a31c9ef
ensarsnr <dev.anise@outlook.com>
2024-01-18 11:03:31 -0500

    Show user information on screen

--- client/src/components/home/SideCardComp.vue
+++ client/src/components/home/SideCardComp.vue
@@ -2,11 +2,11 @@
   <div class="max-w-xs">
     <div class="bg-white shadow-xl rounded-lg py-3">
       <div class="photo-wrapper p-2">
-        <img class="w-32 h-32 bg-black rounded-full mx-auto" />
+        <img class="w-32 h-32 bg-black rounded-full mx-auto" :src="user.profilePhoto" alt="Profil Fotoğrafı" />
       </div>
       <div class="p-2">
         <h3 class="text-center text-xl text-gray-900 font-medium leading-8">
-          {{user.username}}
+          {{ username }}
         </h3>
         <table class="text-xs my-3">
           <tbody>
@@ -14,7 +14,7 @@
               <td class="px-2 py-2 text-gray-500 font-semibold">
                 Paylaşımlar:
               </td>
-              <td class="px-2 py-2">10</td>
+              <td class="px-2 py-2">{{ user.postCount }}</td>
             </tr>
             <tr>
               <td class="px-2 py-2 text-gray-500 font-semibold">Email:</td>
@@ -30,29 +30,43 @@
     </div>
   </div>
 </template>
+
 <script>
-import services from '@/services';
+import services from "@/services";
 
 export default {
-    data() {
-        return {
-            user: {}
-        }
-    },
-    mounted() {
-      this.profile()
-    },
-    methods: {
-  async profile() {
-    try {
-      const response = await services.profile(); 
-      this.user = response.data.user; 
-      console.log(`Profile: ${response}`);
-    } catch (error) {
-      console.log(`Get profile: ${error}`);
-    }
+  data() {
+    return {
+      user: {
+        profilePhoto: "", // Profil fotoğrafı
+        postCount: 0, // Paylaşımlar sayısı
+        email: "", // Email adresi
+      },
+      username: "",
+    };
+  },
+  mounted() {
+    this.profile().then(data => {
+      this.username = data.username;
+      this.user = {
+        email: data.email,
+      };
+    });
+  },
+
+  methods: {
+    async profile() {
+  try {
+    const response = await services.profile();
+    console.log(response)
+    return response.data.user; 
+
+  } catch (error) {
+    console.log(`Get profile: ${error}`);
+    throw error;
   }
 },
 
+  },
 };
 </script>
