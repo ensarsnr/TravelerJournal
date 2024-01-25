e61afa5808630895184fbf484e214af8f2306f81
ensarsnr <dev.anise@outlook.com>
2024-01-25 14:31:03 -0500

    Added component in home page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -21,13 +21,21 @@
       </div>
     </div>
   </div>
+  <div v-if="showModal">
+    <AddPostComp :exitModal="exitModal" />
+  </div>
+  <div @click="showModal = true" class='fixed bottom-0 w-full'>
+    <button class='bottom-0 my-8 float-right mr-5 h-16 w-16 bg-blue-400 text-white text-4xl  tracking-wide rounded-full focus:outline-none'>+</button>
+</div>
 </template>
 
 <script>
 import NavbarComp from "../components/home/NavbarComp.vue";
 import SideCard from "../components/home/SideCardComp.vue";
+import AddPostComp from "@/components/home/AddPostComp.vue";
+
 export default {
-  components: { NavbarComp, SideCard },
+  components: { NavbarComp, SideCard, AddPostComp },
 
   mounted() {
     if (localStorage.getItem("reloaded")) {
@@ -41,7 +49,14 @@ export default {
   data() {
     return {
       count: true,
+      showModal: false,
     };
+  },
+
+  methods: {
+    exitModal () {
+      this.showModal = !this.showModal;
+    }
   },
 
   beforeRouteEnter(to, from, next) {
