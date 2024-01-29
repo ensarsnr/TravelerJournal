bae1854e599f51d1784c23aee61a9a80dd9b66bc
ensarsnr <dev.anise@outlook.com>
2024-01-29 09:59:48 -0500

    Updated prewiev

--- client/src/components/home/AddPostComp.vue
+++ client/src/components/home/AddPostComp.vue
@@ -10,12 +10,16 @@
     ></div>
     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
       <div
+      
         class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
       >
         <div
           class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
         >
-          <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div 
+          v-if="preview"
+
+          class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start">
               <div class="flex items-center justify-center w-full">
                 <label
@@ -50,18 +54,21 @@
                   </div>
                   <input
                     :v-model="image"
+                    v-if="!image"
                     @change="onChangeImage"
                     id="dropzone-file"
                     type="file"
                     class="hidden"
                   />
                   <!-- Eğer image boş değilse ve geçerli bir resim varsa önizlemeyi göster -->
-                  <img
-                    v-if="image"
-                    :src="image"
+                  <div
+                    v-else
                     alt="Preview"
-                    class="object-contain"
-                  />
+                    class="text-center"
+                  >
+                  <div class="mb-5">Resim seçildi önizlemek için buttona tıkla.</div>
+                  <button @click="clickPreview" class="px-4 py-2 rounded-lg text-white bg-gray-300">Önizleme</button>
+                </div>
                 </label>
               </div>
               <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
@@ -72,23 +79,39 @@
                 <div class="mt-2"></div>
               </div>
             </div>
+            
           </div>
-          <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-            <button
-              type="button"
-              class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
-            >
-              Gönder
-            </button>
-            <button
-              @click="exitModal"
-              type="button"
-              class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-            >
-              İptal et
-            </button>
+          <div class="w-full h-2/4" v-else>
+          <img :src="image" class="w-full h-2/4 object-cover"/>
+</div>
+          <div class="bg-gray-50 px-4 py-3 float-end grid grid-cols-2">
+            <div class="flex justify-center bg">
+              <input
+                class="m-auto w-full bg-gray-200 rounded-lg p-2"
+                placeholder="Hakkında"
+              />
+            </div>
+            <div class="flex justify-around">
+              <button
+                type="button"
+                class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+              >
+                Gönder
+              </button>
+              <button
+                @click="exitModal"
+                type="button"
+                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              >
+                İptal et
+              </button>
+            </div>
+        
           </div>
+        
         </div>
+        
+
       </div>
     </div>
   </div>
@@ -99,6 +122,7 @@ export default {
   props: ["exitModal"],
   data() {
     return {
+      preview: true,
       image: "",
     };
   },
@@ -110,6 +134,9 @@ export default {
         console.log(this.image);
       }
     },
+    clickPreview() {
+      this.preview = false
+    }
   },
 };
 </script>
