928f3eb30d066193e43d959a604d64c5de13c53b
ensarsnr <dev.anise@outlook.com>
2024-01-30 15:53:02 -0500

    Added images list

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -8,12 +8,15 @@
           <SideCard />
         </div>
         <div class="col-span-7 p-4">
-          <div class="flex flex-col w-4/5 bg-black">
+          <div v-for="image in images" :key="image" class="mt-5 flex flex-col w-4/5 bg-black">
             <div class="flex">
               <div class="m-1">pp</div>
               <div class="m-1">kullanıcıadı</div>
             </div>
-            <div class="h-52 bg-orange-400">IMG</div>
+            <div class="h-52 bg-orange-400">
+              <!-- <img :src="`../../../server/src/uploads/1706453389062.jpeg`" /> -->
+              <p>{{ image.image }}</p>
+            </div>
             <div>beğenme, kaydetme</div>
             <div>beğeni saysı</div>
           </div>
@@ -37,17 +40,20 @@
 import NavbarComp from "../components/home/NavbarComp.vue";
 import SideCard from "../components/home/SideCardComp.vue";
 import AddPostComp from "@/components/home/AddPostComp.vue";
+import services from "@/services";
 
 export default {
   components: { NavbarComp, SideCard, AddPostComp },
 
   mounted() {
+    this.getImages();
     if (localStorage.getItem("reloaded")) {
       localStorage.removeItem("reloaded");
     } else {
       localStorage.setItem("reloaded", "1");
       location.reload();
     }
+    
   },
 
   data() {
@@ -62,6 +68,15 @@ export default {
     exitModal() {
       this.showModal = !this.showModal;
     },
+    async getImages () {
+      try {
+        const response = await services.getImages();
+        this.images = response.data.images;
+        console.log(response.data.images[0]);
+      } catch (error) {
+        console.log(`getImages error client: ${error}`)
+      }
+    }
   },
 
   beforeRouteEnter(to, from, next) {
