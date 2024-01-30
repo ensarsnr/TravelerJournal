9b99be9e4f92ed1040ffbdbe48916d657f9a5e15
ensarsnr <dev.anise@outlook.com>
2024-01-30 15:52:23 -0500

    New services added

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,7 +1,9 @@
 import register from "./auth/register";
 import login from "./auth/login";
 import profile from "./user/profile";
+import uploadImage from "./user/uploadImage";
+import getImages from "./user/getImages";
 
-const services = { register, login, profile };
+const services = { register, login, profile, uploadImage, getImages };
 
 export default services;
