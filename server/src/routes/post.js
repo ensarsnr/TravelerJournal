735a6e43c8bf207cb215dd637815d3b7428fb5ba
ensarsnr <dev.anise@outlook.com>
2024-01-30 15:54:05 -0500

    New routes added

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const image = require("../controllers/images");
+const getImage = require("../controllers/getImages");
 const authMiddleware = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.post("/image", authMiddleware, image);
+router.get("/get-image", getImage);
 
 module.exports = router;
