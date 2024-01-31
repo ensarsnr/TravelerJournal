e814d3ddf8b9dd88f4aab91da58fdd4f7a54ecf4
ensarsnr <dev.anise@outlook.com>
2024-01-31 15:16:47 -0500

    Get user info

--- server/src/controllers/getImages.js
+++ server/src/controllers/getImages.js
@@ -1,9 +1,16 @@
 const Post = require("../models/post");
+const User = require("../models/user");
 
 const getImages = async (req, res) => {
   try {
-    const images = await Post.find({});
-    res.status(200).json({ images });
+    const posts = await Post.find({}).populate("user"); // Kullanıcının bilgilerini içeren postları çek
+
+    // Eğer post bulunamazsa, 404 hatası döndür
+    if (!posts || posts.length === 0) {
+      return res.status(404).json({ error: "No posts found" });
+    }
+
+    res.status(200).json({ posts });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
