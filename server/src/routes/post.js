62a8ce18b8609e8156be1a635259ae9e9f1cd04c
ensarsnr <dev.anise@outlook.com>
2024-02-13 00:17:05 +0300

    New routes added for delete post

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const image = require("../controllers/images");
 const getImage = require("../controllers/getImages");
+const deleteImage = require("../controllers/deleteImages");
 const authMiddleware = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.post("/image", authMiddleware, image);
+router.post("/delete", authMiddleware, deleteImage);
 router.get("/get-image", getImage);
 
 module.exports = router;
