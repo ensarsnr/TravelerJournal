7e013b225829a49bcb38fb43811039333a215e0c
ensarsnr <dev.anise@outlook.com>
2024-02-14 01:34:52 +0300

    Updated

--- client/src/services/user/profile.js
+++ client/src/services/user/profile.js
@@ -1,12 +1,12 @@
 import api from "../api";
 
 const profile = async () => {
-    try {
-        const response = await api.get("/get/profile");
-        return response;
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const response = await api.get("/get/profile");
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default profile;
