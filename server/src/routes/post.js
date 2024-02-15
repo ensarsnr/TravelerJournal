bc37439fbbf73661e6efa5ecd3163e502b6216a0
ensarsnr <dev.anise@outlook.com>
2024-02-16 00:28:49 +0300

    Added saved route

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -2,11 +2,12 @@ const express = require("express");
 const image = require("../controllers/images");
 const getImage = require("../controllers/getImages");
 const deleteImage = require("../controllers/deleteImages");
+const savedImage = require("../controllers/savedImage");
 const authMiddleware = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.post("/image", authMiddleware, image);
 router.post("/delete", authMiddleware, deleteImage);
 router.get("/get-image", getImage);
-
+router.post("/saved", authMiddleware, savedImage);
 module.exports = router;
